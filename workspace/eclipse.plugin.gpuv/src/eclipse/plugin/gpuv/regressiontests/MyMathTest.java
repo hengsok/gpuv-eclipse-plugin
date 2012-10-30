46405c4debbbb3fc1d5db391540849ffe9f4586f
Heng Sok <hs4110@doc.ic.ac.uk>
2012-10-30 12:21:54 +0000

    added tests junit

---
+++ workspace/eclipse.plugin.gpuv/src/eclipse/plugin/gpuv/regressiontests/MyMathTest.java
@@ -0,0 +1,15 @@
+package eclipse.plugin.gpuv.regressiontests;
+
+import math.MyMath;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyMathTest {
+  @Test
+  public void testMulti() {
+    MyMath math = new MyMath();
+    assertEquals(50, math.multi(5, 10));
+  }
+} 
