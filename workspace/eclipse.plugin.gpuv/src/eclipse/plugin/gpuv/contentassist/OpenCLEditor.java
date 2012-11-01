2374a3328807b59c94729880f7ddb3fc0aeae752
Myung Lee <msl09@fusion33.doc.ic.ac.uk>
2012-11-01 12:13:32 +0000

    auto suggestions package added

---
+++ workspace/eclipse.plugin.gpuv/src/eclipse/plugin/gpuv/contentassist/OpenCLEditor.java
@@ -0,0 +1,20 @@
+package eclipse.plugin.gpuv.contentassist;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+public class OpenCLEditor extends TextEditor {
+
+	private ColorManager colorManager;
+
+	public OpenCLEditor() {
+		super();
+		colorManager = new ColorManager();
+		setSourceViewerConfiguration(new XMLConfiguration(colorManager));
+		setDocumentProvider(new XMLDocumentProvider());
+	}
+	public void dispose() {
+		colorManager.dispose();
+		super.dispose();
+	}
+
+}
