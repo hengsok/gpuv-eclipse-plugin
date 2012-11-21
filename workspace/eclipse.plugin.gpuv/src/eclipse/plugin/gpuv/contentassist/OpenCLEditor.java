fb2b7b40c953b28cee08c4e550cf1629b2ec270b
Yuxiang Zhou <mr.yuxiang.zhou@googlemail.com>
2012-11-21 14:54:59 +0000

    original editor

--- workspace/eclipse.plugin.gpuv/src/eclipse/plugin/gpuv/contentassist/OpenCLEditor.java
+++ workspace/eclipse.plugin.gpuv/src/eclipse/plugin/gpuv/contentassist/OpenCLEditor.java
@@ -1,19 +1,19 @@
 package eclipse.plugin.gpuv.contentassist;
 
-import org.eclipse.cdt.internal.ui.editor.CEditor;
+import org.eclipse.ui.editors.text.TextEditor;
 
-public class OpenCLEditor extends CEditor {
+public class OpenCLEditor extends TextEditor {
 
 	private ColorManager colorManager;
 
 	public OpenCLEditor() {
 		super();
-		//colorManager = new ColorManager();
-        //setDocumentProvider(new OpenCLDocumentProvider());
-        //setSourceViewerConfiguration(new OpenCLConfiguration(colorManager));
+		colorManager = new ColorManager();
+        setDocumentProvider(new OpenCLDocumentProvider());
+        setSourceViewerConfiguration(new OpenCLConfiguration(colorManager));
 	}
 	public void dispose() {
-		//colorManager.dispose();
+		colorManager.dispose();
 		super.dispose();
 	}
 
