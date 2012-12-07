857da7c2fcf33428f41fb3477df29c941c333168
Heng Sok <hs4110@ic.ac.uk>
2012-12-07 18:24:46 +0000

    make it work on both wins and linux now added binary

---
+++ workspace/eclipse.plugin.gpuv/GPUVerifyBinary/linux/libclc/nvptx--nvidiacl/lib/geometric/cross.cl.bc.d
@@ -0,0 +1,52 @@
+nvptx--nvidiacl/lib/geometric/cross.cl.bc: generic/lib/geometric/cross.cl \
+  generic/include/clc/clc.h generic/include/clc/clcfunc.h \
+  generic/include/clc/clcmacro.h generic/include/clc/clctypes.h \
+  generic/include/clc/convert.h generic/include/clc/as_type.h \
+  generic/include/clc/workitem/get_global_size.h \
+  generic/include/clc/workitem/get_global_id.h \
+  generic/include/clc/workitem/get_local_size.h \
+  generic/include/clc/workitem/get_local_id.h \
+  generic/include/clc/workitem/get_num_groups.h \
+  generic/include/clc/workitem/get_group_id.h \
+  generic/include/clc/math/cos.h \
+  generic/include/clc/math/unary_intrin.inc \
+  generic/include/clc/math/exp.h generic/include/clc/math/exp2.h \
+  generic/include/clc/math/fabs.h generic/include/clc/math/floor.h \
+  generic/include/clc/math/fma.h \
+  generic/include/clc/math/ternary_intrin.inc \
+  generic/include/clc/math/hypot.h generic/include/clc/math/gentype.inc \
+  generic/include/clc/math/hypot.inc generic/include/clc/math/log.h \
+  generic/include/clc/math/log2.h generic/include/clc/math/mad.h \
+  generic/include/clc/math/mad.inc generic/include/clc/math/pow.h \
+  generic/include/clc/math/binary_intrin.inc \
+  generic/include/clc/math/sin.h generic/include/clc/math/sqrt.h \
+  generic/include/clc/math/native_cos.h \
+  generic/include/clc/math/native_divide.h \
+  generic/include/clc/math/native_exp.h \
+  generic/include/clc/math/native_exp2.h \
+  generic/include/clc/math/native_log.h \
+  generic/include/clc/math/native_log2.h \
+  generic/include/clc/math/native_powr.h \
+  generic/include/clc/math/native_sin.h \
+  generic/include/clc/math/native_sqrt.h \
+  generic/include/clc/math/rsqrt.h generic/include/clc/integer/abs.h \
+  generic/include/clc/integer/gentype.inc \
+  generic/include/clc/integer/abs.inc \
+  generic/include/clc/integer/abs_diff.h \
+  generic/include/clc/integer/abs_diff.inc \
+  generic/include/clc/integer/add_sat.h \
+  generic/include/clc/integer/add_sat.inc \
+  generic/include/clc/integer/sub_sat.h \
+  generic/include/clc/integer/sub_sat.inc \
+  generic/include/clc/geometric/cross.h \
+  generic/include/clc/geometric/dot.h \
+  generic/include/clc/geometric/floatn.inc \
+  generic/include/clc/geometric/dot.inc \
+  generic/include/clc/geometric/length.h \
+  generic/include/clc/geometric/length.inc \
+  generic/include/clc/geometric/normalize.h \
+  generic/include/clc/geometric/normalize.inc \
+  generic/include/clc/relational/any.h \
+  generic/include/clc/relational/select.h \
+  generic/include/clc/synchronization/cl_mem_fence_flags.h \
+  generic/include/clc/synchronization/barrier.h
---
+++ workspace/eclipse.plugin.gpuv/GPUVerifyBinary/windows/GPUVerify.bat
@@ -0,0 +1,3 @@
+@echo off
+
+python %~dp0GPUVerify.py %*       
